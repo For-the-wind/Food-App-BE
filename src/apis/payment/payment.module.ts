03d5cb7253fc5f1d473d559511aba5dc3f38a083
Tran Viet Cuong <rinjin2004@gmail.com>
2026-07-10 09:59:25 +0700

    ref: change name file

--- src/apis/payment/payment.module.ts
+++ src/apis/payment/payment.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
-import { AlepayController } from './payment.controller';
+import { AlepayController } from './alepay.controller';
 import { AlepayService } from './alepay.service';
 
 @Module({
